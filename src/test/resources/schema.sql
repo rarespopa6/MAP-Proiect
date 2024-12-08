972d7342aa8b1c16a7bac36e399441d6e28b3842
Darius Bolos <63288528+DariusBolos@users.noreply.github.com>
2024-12-08 18:45:08 +0200

    finished implemented tests

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE account (
+CREATE TABLE IF NOT EXISTS account (
                          id INT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(255) NOT NULL,
                          balance DECIMAL(10, 2) NOT NULL
